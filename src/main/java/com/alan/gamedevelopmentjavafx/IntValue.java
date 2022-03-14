ce943b179731ff32b52e9681e47e3e00edc20cc5
Alannegra <alanrodriguez@elpuig.xeill.net>
2022-03-14 18:37:12 +0100

    animacion rotate

---
+++ src/main/java/com/alan/gamedevelopmentjavafx/IntValue.java
@@ -0,0 +1,17 @@
+package com.alan.gamedevelopmentjavafx;
+
+public class IntValue {
+    public int value;
+
+    public IntValue(int i) {
+        value = i;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setPoints(int value) {
+        this.value = value;
+    }
+}
--- src/main/java/com/alan/gamedevelopmentjavafx/Keyboard.java
+++ src/main/java/com/alan/gamedevelopmentjavafx/Keyboard.java
@@ -1,17 +1,30 @@
 package com.alan.gamedevelopmentjavafx;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 public class Keyboard extends Application {
 
@@ -25,6 +38,8 @@ public class Keyboard extends Application {
 
         Canvas canvas = new Canvas( 1980, 1080 );
         root.getChildren().add( canvas );
+
+        final int[] points = {0};
 
         ArrayList<String> input = new ArrayList<String>();
 
@@ -59,17 +74,32 @@ public class Keyboard extends Application {
         Image right = new Image( "Garroshinvert.gif" );
         Image axe = new Image( "Gorehowl.png" );
         Image dehaka = new Image("space.png");
+        Sprite badguy = new Sprite();
+        Sprite weapon = new Sprite();
+        weapon.setImage(axe);
+        badguy.setImage(dehaka);
+
+
+        Font theFont = Font.font( "Helvetica", FontWeight.BOLD, 24 );
+        gc.setFont( theFont );
+        gc.setStroke( Color.BLACK );
+        gc.setLineWidth(1);
 
 
         new AnimationTimer()
         {
             boolean derechaizquietda = false;
+
             int x = 0;
             int y = 0;
+            int axex= 0;
             int xrandom = 0;
             int yrandom = 0;
             boolean rightleft = true;
             boolean topdown = true;
+            boolean muerto = true;
+            Rotate rotate;
+
             public void handle(long currentNanoTime)
             {
 
@@ -78,14 +108,39 @@ public class Keyboard extends Application {
 
                 gc.drawImage( background, 0, 0,1980,1080);
 
-                if(input.contains("F")){
-                    gc.drawImage( axe, x + 10, y + 10 );
-                }
 
+                if(input.contains("F")){
+                    //gc.drawImage( axe, x + axex, y + 10 );
+
+                    //gc.rotate(weapon.getBoundary().getHeight()+x);
+
+                    Rotate rotate = new Rotate(180,x,y);
+                    //rotate.setAngle(20);
+                    //rotate.setPivotX(150);
+                    //rotate.setPivotY(225);
+                    weapon.getTransforms().add(rotate);
+
+                    //weapon.setPosition(x + axex,y + 10);
+                    weapon.render(gc);
+
+                    if(axex == 100)axex=0;
+                    axex+=5;
+                    if (weapon.intersects(badguy)){
+                        muerto = false;
+                        points[0]++;
+                    }
+                }
 
                 xrandom = randomx();
                 yrandom = randomy();
-                gc.drawImage(dehaka, xrandom ,yrandom);
+
+
+                if(muerto){
+                    badguy.setPosition(xrandom ,yrandom);
+                    badguy.render(gc);
+                    //gc.drawImage(dehaka, xrandom ,yrandom);
+                }
+
 
 
                 if(x == 1600 ) x-=5;
@@ -160,7 +215,14 @@ public class Keyboard extends Application {
                     gc.drawImage( left, x, y );
                 }
 
+                gc.setFill( Color.RED );
+                String pointsText = "Points: " + points[0];
+                gc.fillText( pointsText, 360, 36 );
+
+                gc.strokeText( pointsText, 360, 36 );
+
                 }
+
 
                 int randomx (){
                 if(xrandom == 1750 ){
@@ -179,9 +241,16 @@ public class Keyboard extends Application {
                 if(topdown)return yrandom+5;
                 else return yrandom-5;
 
+
+
             }
 
+
+
         }.start();
+
+
+
 
         theStage.show();
     }
